450b0f016fc8b93ec476bd067ec121263eb0910b
mossberg500 <axaptasub@gmail.com>
2021-03-24 21:09:58 +0300

    добавили отображение категорий слева

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from '../../service/data-handler.service';
+import {Category} from '../../model/Category';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  categories: Category[];
 
-  constructor() { }
+  constructor(private dataHandler: DataHandlerService) {
+  }
 
+  // метод вызывается автоматически после инициализации компонента
   ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
+   // console.log(this.catagories);
   }
 
 }
