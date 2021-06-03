42af28a8dff58100e02171a3fbde6c4b4a015da6
mossberg500 <axaptasub@gmail.com>
2021-06-03 17:31:11 +0300

    first 0306

---
+++ src/app/views/tasks/tasks.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {Task} from 'src/app/model/Task';
+import {DataHandlerService} from '../../service/data-handler.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[];
+
+  constructor(private dataHandler: DataHandlerService) {
+  }
+
+  ngOnInit(): void {
+    this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+  }
+
+  toggleTaskCompleted(task: Task) {
+    task.completed = !task.completed;
+  }
+}
